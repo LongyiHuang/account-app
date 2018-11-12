cb7f9783ff6f56185cd9706ee6fd643d09d6432f
huanglongyi <475865662@qq.com>
2018-11-12 09:58:22 +0800

    Updates

---
+++ precache-manifest.9458b433c0054b214016cc0cdf10646d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1d149ffdab999100ecf",
+    "url": "/account-app/static/js/runtime~main.e1d149ff.js"
+  },
+  {
+    "revision": "bbb61bd6268148c7e3e1",
+    "url": "/account-app/static/js/main.bbb61bd6.chunk.js"
+  },
+  {
+    "revision": "2b9aec365961ae4f8625",
+    "url": "/account-app/static/js/1.2b9aec36.chunk.js"
+  },
+  {
+    "revision": "bbb61bd6268148c7e3e1",
+    "url": "/account-app/static/css/main.5c3b3b4f.chunk.css"
+  },
+  {
+    "revision": "e3931589064be0c9b1f5d10d4bfb14a0",
+    "url": "/account-app/index.html"
+  }
+];
